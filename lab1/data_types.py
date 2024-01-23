eb035c2f0225961cab1ff4103a001dcb88cd5028
yasmin846 <156778403+yasmin846@users.noreply.github.com>
2024-01-23 15:49:08 +0600

    Add files via upload

---
+++ lab1/data_types.py
@@ -0,0 +1,27 @@
+#1
+x = 5
+print(type(x)) #will print int
+
+#2
+x = "Hello World"
+print(type(x)) #will print str
+
+#3
+x = 20.5
+print(type(x)) #will print float
+
+#4
+x = ["apple", "banana", "cherry"]
+print(type(x)) #will print list
+
+#5
+x = ("apple", "banana", "cherry")
+print(type(x)) #will print tuple
+
+#6
+x = {"name" : "John", "age" : 36}
+print(type(x)) #will print dict
+
+#7
+x = True
+print(type(x)) #will print bool
