73b62bfe1095cee8d9980e274db133a0cc4d490d
yasmin846 <156778403+yasmin846@users.noreply.github.com>
2024-02-12 21:16:58 +0600

    Update classes.py

--- lab3/classes.py
+++ lab3/classes.py
@@ -39,7 +39,39 @@ class square(shape):
         self.length = length
     def area(self):
         print(self.length ** 2)
+class rectangle(shape):
+    def __init__(self, length, width):
+        super().__init__()
+        self.length = length
+        self.width = width
+    def area(self):
+        print(self.length * self.width)
 shape = shape()
-shape.area()
-square = square(int(input()))
-square.area()
+len = int(input())
+wdth = int(input())
+rec = rectangle(len, wdth)
+rec.area()
+
+#4
+class Account():
+    def __init__(self, owner, balance = 0):
+        self.owner = owner
+        self.balance = balance
+    def deposit(self, money):
+        if money>0:
+            self.balance += money
+            print("Your balance: ", self.balance)
+        else:
+            pass
+    def withdraw(self, draw):
+        self.balance -= draw
+        print("Your balance: ", self.balance)
+account = Account("John", 10500)
+print("Summ of transition:")
+d = int(input())
+account.deposit(d)
+print("Pay the draw:")
+w = int(input())
+account.withdraw(w)
+
+#5
