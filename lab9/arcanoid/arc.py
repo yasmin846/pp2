d5fb6c7c93d7f8a021ef15fe37416142c01e5658
yasmin846 <156778403+yasmin846@users.noreply.github.com>
2024-04-10 00:05:00 +0500

    Update arc.py

--- lab9/arcanoid/arc.py
+++ lab9/arcanoid/arc.py
@@ -2,7 +2,7 @@ import pygame
 import random
 pygame.init()
 
-W, H = 1200, 800
+W, H = 1200, 600
 FPS = 60
 
 screen = pygame.display.set_mode((W, H), pygame.RESIZABLE)
@@ -19,7 +19,7 @@ paddle = pygame.Rect(W // 2 - paddleW // 2, H - paddleH - 30, paddleW, paddleH)
 
 #Ball
 ballRadius = 20
-ballSpeed = 6
+ballSpeed = 5
 ball_rect = int(ballRadius * 2 ** 0.5)
 ball = pygame.Rect(random.randrange(ball_rect, W - ball_rect), H // 2, ball_rect, ball_rect)
 dx, dy = 1, -1
@@ -31,8 +31,13 @@ game_score_text = game_score_fonts.render(f'Your game score is: {game_score}', T
 game_score_rect = game_score_text.get_rect()
 game_score_rect.center = (210, 20)
 
+bonus = pygame.Rect(random.randrange(0, W - 100), 200, 100, 50)
+bonus_color = (0, 255, 0)
+bonus_text = "Bonus"
+
 #Catching sound
-collision_sound = pygame.mixer.Sound('audio/catch.mp3')
+collision_sound = pygame.mixer.Sound('catch.mp3')
+bonus_sound = pygame.mixer.Sound('bonus.mp3')
 
 def detect_collision(dx, dy, ball, rect):
     if dx > 0:
@@ -56,9 +61,16 @@ def detect_collision(dx, dy, ball, rect):
 #block settings
 block_list = [pygame.Rect(10 + 120 * i, 50 + 70 * j,
         100, 50) for i in range(10) for j in range (4)]
-color_list = [(random.randrange(0, 255), 
-    random.randrange(0, 255),  random.randrange(0, 255))
-              for i in range(10) for j in range(4)] 
+unbreakable = [pygame.Rect(10 + 120 * i, 50 + 70 * 4, 100, 50) for i in range(2, 8)]
+block_list.extend(unbreakable)
+
+color_list = [(random.randint(0,255), random.randint(0,255), random.randint(0,255))
+    for i in range(len(block_list))] 
+
+for i in range(len(block_list)-len(unbreakable), len(block_list)):
+    color_list[i] = (255, 255, 255)
+
+
 print(block_list)
 #Game over Screen
 losefont = pygame.font.SysFont('comicsansms', 40)
@@ -86,7 +98,19 @@ while not done:
      for color, block in enumerate (block_list)] #drawing blocks
     pygame.draw.rect(screen, pygame.Color(255, 255, 255), paddle)
     pygame.draw.circle(screen, pygame.Color(255, 0, 0), ball.center, ballRadius)
-    # print(next(enumerate (block_list)))
+    pygame.draw.rect(screen, bonus_color, bonus)
+
+    if ball.colliderect(bonus):
+        dx, dy = detect_collision(dx, dy, ball, bonus)
+        game_score += 5
+        ballSpeed += 1  
+        bonus = pygame.Rect(random.randrange(0, W - 100), 200, 100, 50)
+        bonus_color = (0, 255, 0)
+        bonus_sound.play()
+
+    font_bonus = pygame.font.SysFont('Arial', 20)
+    text_bonus = font_bonus.render(bonus_text, True, (255, 255, 255))
+    screen.blit(text_bonus, (bonus.x + 25, bonus.y + 10))
 
     #Ball movement
     ball.x += ballSpeed * dx
@@ -101,16 +125,19 @@ while not done:
     #Collision with paddle
     if ball.colliderect(paddle) and dy > 0:
         dx, dy = detect_collision(dx, dy, ball, paddle)
-
+        
     #Collision blocks
     hitIndex = ball.collidelist(block_list)
 
     if hitIndex != -1:
-        hitRect = block_list.pop(hitIndex)
-        hitColor = color_list.pop(hitIndex)
-        dx, dy = detect_collision(dx, dy, ball, hitRect)
-        game_score += 1
-        collision_sound.play()
+        if block_list[hitIndex] in unbreakable:
+            dx, dy = detect_collision(dx, dy, ball, block_list[hitIndex])
+        else:
+            hitRect = block_list.pop(hitIndex)
+            hitColor = color_list.pop(hitIndex)
+            dx, dy = detect_collision(dx, dy, ball, hitRect)
+            game_score += 1
+            collision_sound.play()
         
     #Game score
     game_score_text = game_score_fonts.render(f'Your game score is: {game_score}', True, (255, 255, 255))
